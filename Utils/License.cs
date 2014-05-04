97d52785519c7f09b35822464efcf0ec2d790938
Stanilav Hacker <info@dbsys.cz>
2014-05-04 13:01:07 +0200

    Pridani souboru

---
+++ Utils/License.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Phone.Marketplace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CoPilot.Utils
+{
+    public class License
+    {
+        /// <summary>
+        /// Licence info
+        /// </summary>
+        private static LicenseInformation license = new LicenseInformation();
+
+        /// <summary>
+        /// Is Trial
+        /// </summary>
+        private static bool isTrial = true;
+        public static bool IsTrial
+        {
+            get
+            {
+                return isTrial;
+            }
+        }
+
+        /// <summary>
+        /// Resolve license
+        /// </summary>
+        public static void ResolveLicense() 
+        {
+            isTrial = license.IsTrial();
+        }
+
+    }
+}
